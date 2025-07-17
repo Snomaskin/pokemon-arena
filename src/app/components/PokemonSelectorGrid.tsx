e03a04291c8f2b3f38217587b011f75819c2a794
Snomaskin <mark_0094@icloud.com>
2025-07-17 19:29:15 +0200

    made Home a server component, as it should be

--- src/app/components/PokemonSelectorGrid.tsx
+++ src/app/components/PokemonSelectorGrid.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState, useEffect } from "react";
 import { Pokemon } from "@/utils/fetchPokemon";
 import Image from "next/image";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-"use client";
 import PokemonSelectorGrid from "./components/PokemonSelectorGrid";
-import { type Pokemon, fetchPokemon } from "@/utils/fetchPokemon";
+import { fetchPokemon } from "@/utils/fetchPokemon";
 import pokemonSelection from "./data/pokemonSelection";
-import { useEffect, useState } from "react";
 
 
-export default function Home() {
-  const [pokemons, setPokemons] = useState<Pokemon[]>();
-  useEffect(() => {
-
-    const fetchPokemons = async () => {
-      const promises = pokemonSelection.map(pokemon => fetchPokemon(pokemon));
-      const pokemons = await Promise.all(promises);
-      setPokemons(pokemons);
-    }
-    fetchPokemons();
-  }, [])
+export default async function Home() {
+  const promises = pokemonSelection.map(pokemon => fetchPokemon(pokemon));
+  const pokemons = await Promise.all(promises);
 
   return (
    <main className="flex w-screen min-h-screen bg-amber-100 justify-center">
