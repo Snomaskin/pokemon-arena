4365eb72079fbe576206c729497152a718f93c0b
Snomaskin <mark_0094@icloud.com>
2025-07-21 19:52:59 +0200

    hopefully a solution to vercel build crashing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { fetchPokemon } from "@/utils/fetchPokemon";
 import pokemonsArr from "@/data/pokemonsArr";
 import ClientShell from "./ClientShell";
+import { Pokemon } from "@/types/pokemon";
 
 
 export default async function Home() {
-  const promises = pokemonsArr.map(pokemon => fetchPokemon(pokemon));
-  const pokemons = await Promise.all(promises);
+const pokemons = (await Promise.allSettled(pokemonsArr.map(fetchPokemon)))
+  .map(result => result.status === 'fulfilled' ? result.value : null)
+  .filter((pokemon): pokemon is Pokemon => pokemon !== null);
 
   return (
    <main className="flex w-screen min-h-screen bg-amber-100 justify-center">
