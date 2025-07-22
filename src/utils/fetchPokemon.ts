f90624c4b98bcb327f53546fe61361aa787665f6
Snomaskin <mark_0094@icloud.com>
2025-07-22 14:54:20 +0200

    pokemon caching and image prefetch to limit api requests throughout the app

--- src/utils/fetchPokemon.ts
+++ src/utils/fetchPokemon.ts
@@ -3,12 +3,24 @@ import type { Move, Pokemon } from "@/types/pokemon";
 
 const pokemonApi = new PokemonClient();
 const moveApi = new MoveClient();
+const pokemonCache = new Map<string, Pokemon>();
 
 export const fetchPokemon = async (name: string): Promise<Pokemon> => {
+  if (pokemonCache.has(name)) {
+    return pokemonCache.get(name)!;
+  };
   const data = await pokemonApi.getPokemonByName(name);
 
   const imageUrl = data.sprites.other?.["official-artwork"]?.front_default ?? "";
   const hp = data.stats.find((s) => s.stat.name === "hp")?.base_stat ?? 0;
+
+  let imageBase64 = "";
+  if (imageUrl) {
+    const response = await fetch(imageUrl);
+    const buffer = await response.arrayBuffer();
+    const base64 = Buffer.from(buffer).toString("base64");
+    imageBase64 = `data:image/png;base64,${base64}`;
+  };
 
   const moves: Move[] = await Promise.all(
     data.moves.map(async ({ move }) => {
@@ -23,11 +35,15 @@ export const fetchPokemon = async (name: string): Promise<Pokemon> => {
     })
   );
 
-  return {
+  const pokemon: Pokemon = {
     id: data.id,
     name: data.name,
-    imageUrl,
+    imageUrl: imageBase64,
     moves,
     hp,
   };
+  
+  pokemonCache.set(name, pokemon);
+
+  return pokemon;
 };
