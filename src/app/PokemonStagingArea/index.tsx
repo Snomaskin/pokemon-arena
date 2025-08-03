e200d360f2d97ebd25282e3703d4d0784c3e95ce
Snomaskin <mark_0094@icloud.com>
2025-08-03 15:31:31 +0200

    style: buttons

--- src/app/PokemonStagingArea/index.tsx
+++ src/app/PokemonStagingArea/index.tsx
@@ -12,9 +12,9 @@ export default function PokemonStagingArea() {
     <div className="max-h-fit bg-gradient-to-r from-blue-50 via-purple-50 to-red-50 p-6 mr-3 ml-3 rounded-xl shadow-lg border border-gray-200">
       <div className="flex justify-between sm:items-center mb-3 lg:mb-4 gap-2">
         <h2 className="text-xl lg:text-2xl font-bold text-gray-800">Staging Area</h2>
-        <div className="flex justify-end items-center sm:gap-1 lg:gap-3">
+        <div className="flex flex-col-reverse lg:flex-row justify-end items-center sm:gap-1 lg:gap-3 gap-y-2">
           <button 
-            className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-lg transition-colors duration-200 shadow-md hover:shadow-lg cursor-pointer"
+            className="bg-red-500 hover:bg-red-600 text-white px-3 py-2 rounded-xl transition-colors duration-200 shadow-md hover:shadow-lg cursor-pointer"
             onClick={resetSelection}
           >
             Reset
